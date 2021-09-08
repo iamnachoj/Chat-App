ef89ac1c705f69246ce75cb5991b38f3e28bbfdf
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-09-08 16:56:13 +0200

    remove current code practice

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View >
-      <Text style={[styles.blue, styles.bold]}>Hello World!</Text>
-      <Text style={[styles.red, styles.big]}>How are you?</Text>
-      <Text style={[styles.big, styles.blue, styles.bold]}>I am feeling blue!</Text>
-      <View style={styles.box}></View>
-    </View>
-  );
+export default class HelloWorld extends React.Component{
+  render() {
+    return (
+      <View></View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blue: {
-    color: 'blue'
-  },
-  big: {
-    fontSize: 30
-  },
-  bold: {
-    fontWeight: 'bold'
-  },
-  red: {
-    color: 'red'
-  },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'skyblue'
-  }
 });
