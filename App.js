1ad27fdd2018350849cca29983b1edceda2daaeb
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-09-09 16:36:12 +0200

    add new screen details

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
-export default class HelloWorld extends React.Component{
-  
-  render() {
+//components
+import Home from './screens/home';
+import ReviewDetails from './screens/reviewDetails';
+
+// navigator
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const getFonts = () => {
+  return (
+    Font.loadAsync({
+      'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+      'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    })
+  )}
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+  const Stack = createStackNavigator();
+
+  if(fontsLoaded){
+    return (
+      <NavigationContainer>
+      <Stack.Navigator>
+        {/* it automatically adds a navigation prop */}
+        <Stack.Screen name='Home' component={Home}/> 
+        <Stack.Screen name='Reviews' component={ReviewDetails}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+     <AppLoading
+      startAsync={getFonts}
+      onFinish={() => setFontsLoaded(true)}
+      onError={console.warn}
+      />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-
-});
---
+++ screens/reviewDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function ReviewDetails({navigation}) {
+
+  const pressHandler = () => {
+     navigation.goBack()
+  }
+   return (
+     <View style={globalStyles.container}>
+       <Text>Review detail Screen</Text>
+       <Button title={'Back'} onPress={pressHandler}/>
+     </View>
+   )
+}
