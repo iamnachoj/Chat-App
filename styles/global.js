4d6c468f27915f3aee6029041ba0559446af0902
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-09-11 15:15:04 +0200

    add border radius and box opacity to dashboard container in home screen

--- styles/global.js
+++ styles/global.js
@@ -9,7 +9,17 @@ export const globalStyles = StyleSheet.create({
     height: 260,
     width: 300,
     padding: 10,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    opacity: 0.89,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
   titleText:{
     fontFamily: 'nunito-bold',
