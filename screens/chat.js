83e3267a56cc757cafd69bab29876e8db8319214
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-09-11 18:39:45 +0200

    add comments to ensure everything is clearly explained

--- screens/chat.js
+++ screens/chat.js
@@ -4,12 +4,14 @@ import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import {globalStyles} from '../styles/global';
 
 export default class Chat extends React.Component {
+  // this is just mock data to store messages
   constructor() {
     super();
     this.state = {
        messages: [],
     };
   }
+   // after the chat component is mounted, we store these 2 messages as an example, changing the current state of messages (empty array)
   componentDidMount() {
     this.setState({
       messages: [
@@ -32,7 +34,7 @@ export default class Chat extends React.Component {
       ],
     })
   }
-  
+  // a funtion that changes the color of the bubble of your own messages when you send them
   renderBubble(props) {
     return (
       <Bubble
@@ -45,28 +47,35 @@ export default class Chat extends React.Component {
       />
     )
   }
-
+  // appends new messages to the current list of messages.
   onSend(messages = []) {
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
   }
   render() {
-    
+      // takes name and bgColor from props from the navigation from home screen to use them in this component
       let name = this.props.route.params.name;
       let bgColor = this.props.route.params.bgColor;
+      //this line bellow changes the title of the screen to "name", the variable taken from props from home screen
       this.props.navigation.setOptions({title: name});
 
       return (
+        // full view
       <View  style={{flex: 1, backgroundColor: bgColor, paddingBottom: 20, paddingHorizontal: 10}}>
+        {/* Gifted chat is a component taken from the dependency, it has its own methods. check documentation for further explanation */}
         <GiftedChat
+        // call the function mentioned above
         renderBubble={this.renderBubble.bind(this)}
+        // store messages
         messages={this.state.messages}
+        // onsend function
         onSend={(messages) => this.onSend(messages)}
         user={{
           _id: 1,
         }}
       />
+       {/* this line bellow makes sure that any android version does not have issues with the keyboard taking the screen room and hidding the chat. */}
       { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
       )
--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, TextInput, Button, ImageBackground, TouchableOpacity } from 'react-native';
-import {globalStyles} from '../styles/global';
 
-const image = { uri: "" };
+
+//All styles are in the folder styles, on an object called globalStyles. 
+import {globalStyles} from '../styles/global';
 
 export default class Screen1 extends React.Component {
   constructor(props){
@@ -11,20 +12,27 @@ export default class Screen1 extends React.Component {
   }
   render() {
    return (
+     // an imageBackground covers the rest of the code. The image is required from assets folder. 
      <ImageBackground source={require("../assets/bg.png")} style={globalStyles.image}>
      <View style={globalStyles.container}>
+     {/* dashboard view that contains the functionality in a white box above the image. */}
        <View style={globalStyles.dashboardContainer}>
         <Text style={globalStyles.titleText}>Hello World!</Text>
+        {/* A text input that changes the name state to the value of it. */}
         <TextInput
           style={globalStyles.input}
           placeholder='Your name'
           onChangeText={(name) => this.setState({name})}
           value={this.state.name}
         />
+        {/* Color selection */}
          <View style={globalStyles.colorPickerContainer}>
-              <Text style={globalStyles.chooseColor}>Choose Background Color:</Text>
+              <Text style={globalStyles.chooseColor}
+              accessibilityLabel="More options"
+              accessibilityHint="Let’s you choose a color"
+              >Choose Background Color:</Text>
               <View style={globalStyles.colorPicker}>
-                <TouchableOpacity
+                <TouchableOpacity 
                   style={[globalStyles.colors, globalStyles.blue]}
                   onPress={() => this.setState({ bgColor: "#BED7FF" })}
                 ></TouchableOpacity>
@@ -42,6 +50,7 @@ export default class Screen1 extends React.Component {
                 ></TouchableOpacity>
               </View>
             </View>
+            {/*  Button that navigates to the chat via React navigation */}
         <Button title={'Go to Chat'} onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name, bgColor: this.state.bgColor})}/>
        </View>
      </View>
